1258abca76094e8ed7f88d728c44af1210ebaae2
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-11 01:27:14 +0000

    FINAL

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ speech.continuous = true
 speech.interimResults = false
 
 function setup() {
-  createCanvas(windowWidth, windowWidth)
+  createCanvas(windowWidth, windowHeight)
   background(255)
   fill(25)
 
@@ -11,7 +11,7 @@ function setup() {
   textAlign(CENTER)
   textStyle(BOLDITALIC)
   textFont('"Avenir Next", system-ui, sans-serif')
-  text('SAY A COLOR', width / 2, height / 2)
+  text('SAY SOMETHING', width / 2, height / 2)
   speech.start()
 }
 
@@ -21,10 +21,7 @@ function draw() {
 
 function parseResult() {
   if (speech.resultValue) {
-    const color = speech.resultString
-      .split(' ')
-      .pop()
-      .toUpperCase()
+    const color = speech.resultString.split(' ').pop().toUpperCase()
     background(color)
     text(color, width / 2, height / 2)
     console.log(color)
